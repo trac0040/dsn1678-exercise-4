dc01b1b277f2222a28bb23d0191b807fd9d02a4f
Marta <trac0040@algonquinlive.com>
2014-10-09 10:02:53 -0400

    Added movement and opacity to shuttle

--- js/main.js
+++ js/main.js
@@ -3,6 +3,8 @@ var $sun = $('.sun');
 var $sunSection = $('.sun-section');
 var $dipperSection = $('.dipper-section')
 var $dipper = $('.dipper')
+var $shuttle = $('.shuttle')
+var $shuttleSection = $('.shuttle-section')
 
 $win.on('scroll', function () {
 	var scrollPos = $win.scrollTop();
@@ -14,3 +16,7 @@ $win.on('scroll', function () {
 $dipperSection.waypoint(function () {
 	$dipper.addClass('js-dipper-fade');
 }, { offset: '50%' });
+
+$shuttleSection.waypoint(function () {
+	$shuttle.addClass('js-shuttle-up');
+}, {offset: '50%' });
