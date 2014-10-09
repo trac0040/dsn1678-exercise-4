92247c3d0f5c40a3d8f67bc3372e88b854ce4c45
Marta <trac0040@algonquinlive.com>
2014-10-09 09:43:52 -0400

    Added animations, added shuttle

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 var $win = $(window);
 var $sun = $('.sun');
 var $sunSection = $('.sun-section');
+var $dipperSection = $('.dipper-section')
+var $dipper = $('.dipper')
 
 $win.on('scroll', function () {
 	var scrollPos = $win.scrollTop();
@@ -8,3 +10,7 @@ $win.on('scroll', function () {
 	$sun.css('transform', 'rotate(' + scrollPos / 5 + 'deg)');
 	$sunSection.css('background-position', 'center ' + scrollPos / 2 + 'px');
 })
+
+$dipperSection.waypoint(function () {
+	$dipper.addClass('js-dipper-fade');
+}, { offset: '50%' });
