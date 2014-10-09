0a1ce26b4ff845570f2c491bfbdbae27a5422c49
Marta <trac0040@algonquinlive.com>
2014-10-09 08:52:24 -0400

    Added parallax

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 var $win = $(window);
 var $sun = $('.sun');
+var $sunSection = $('.sun-section');
 
 $win.on('scroll', function () {
 	var scrollPos = $win.scrollTop();
 	
-	$sun.css('transform', 'rotate(' + scrollPos + 'deg)');
+	$sun.css('transform', 'rotate(' + scrollPos / 5 + 'deg)');
+	$sunSection.css('background-position', 'center ' + scrollPos / 2 + 'px');
 })
