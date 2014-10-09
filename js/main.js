e6c0f46b2a17ed525c6ae965898d4f60c279e15b
Marta <trac0040@algonquinlive.com>
2014-10-09 10:49:57 -0400

    added ships

--- js/main.js
+++ js/main.js
@@ -5,12 +5,14 @@ var $dipperSection = $('.dipper-section')
 var $dipper = $('.dipper')
 var $shuttle = $('.shuttle')
 var $shuttleSection = $('.shuttle-section')
+var $shipSection = $('.ship-section');
 
 $win.on('scroll', function () {
 	var scrollPos = $win.scrollTop();
 	
 	$sun.css('transform', 'rotate(' + scrollPos / 5 + 'deg)');
 	$sunSection.css('background-position', 'center ' + scrollPos / 2 + 'px');
+	$shipSection.css('backround-position', 'center ' + scrollPos / 2 + 'px');
 })
 
 $dipperSection.waypoint(function () {
